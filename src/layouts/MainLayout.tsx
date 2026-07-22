7c2ac90a2a82c93ae69dea03ea70ce8b822c9490
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-07-22 15:41:02 +0800

    LEARN: passing props from parent to children using <Outlet />

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { useState } from "react";
 
 export default function MainLayout() {
+  const [count, setCount] = useState<number>(0);
+
+  const handleDecrementCount = () => {
+    setCount((n) => n - 1);
+  };
+
+  const handleIncrementCount = () => {
+    setCount((n) => n + 1);
+  };
+
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Outlet
+        context={{
+          count,
+          handleIncrementCount,
+          handleDecrementCount,
+        }}
+      />
     </>
   );
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,21 @@
+import { useOutletContext } from "react-router-dom";
+
+export type TypeOutletCountState = {
+  count: number;
+  handleDecrementCount: () => void;
+  handleIncrementCount: () => void;
+};
+
 export default function LandingPage() {
+  const { count, handleIncrementCount, handleDecrementCount } =
+    useOutletContext<TypeOutletCountState>();
+
   return (
     <div>
       <h3>This is landing page.</h3>
+      <button onClick={handleDecrementCount}>-</button>
+      <input type="number" value={count} />
+      <button onClick={handleIncrementCount}>+</button>
     </div>
   );
 }
