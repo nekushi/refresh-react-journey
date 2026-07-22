b1786da6664ce0c39f4e4c65bff3f47caaa794c1
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-07-22 15:30:56 +0800

    LEARN: tries to imitate layout.tsx of nextjs. trying react layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Settings from "./pages/Settings";
+import MainLayout from "./layouts/MainLayout";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route element={<MainLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
