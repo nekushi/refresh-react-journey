12a42aa68e7d01adcb39faad51e636d7de834320
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-07-22 15:43:34 +0800

    LEARN: routing to sub-pages. still using <Outlet />

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Settings from "./pages/Settings";
 import MainLayout from "./layouts/MainLayout";
+import DeepSettings from "./pages/sub-pages/DeepSettings";
 
 export function App() {
   return (
@@ -9,7 +10,9 @@ export function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route index element={<LandingPage />} />
-          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings" element={<Settings />}>
+            <Route path="deep-settings" element={<DeepSettings />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/sub-pages/DeepSettings.tsx
@@ -0,0 +1,7 @@
+export default function DeepSettings() {
+  return (
+    <div>
+      <h3>This is deep settings page.</h3>
+    </div>
+  );
+}
