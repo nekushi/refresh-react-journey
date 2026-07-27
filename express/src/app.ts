af7ec15e47a89b09e2d5b979e9cd9c80c15a54c6
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-07-27 19:04:13 +0800

    LEARN: understanding express routing

--- express/src/app.ts
+++ express/src/app.ts
@@ -3,9 +3,12 @@ import express, { type Express, type Request, type Response } from "express";
 const app: Express = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello world");
-});
+app.get(
+  "/name/:name/*placeholder",
+  (req: Request<{ name: string; placeholder: string[] }>, res: Response) => {
+    res.send(req.params.placeholder.join("/"));
+  },
+);
 
 app.listen(port, () => {
   console.log(`Server running at port ${port}`);
