a9af7eab45175b7567b0d33a09cd02ccf10bc61f
Ivan Christian Pabayo <zzzpabayo2@gmail.com>
2026-08-13 00:13:15 +0800

    FEAT: idk, i lost track of which type is which, so i created an auth.type.ts

--- src/types/users.type.ts
+++ src/types/users.type.ts
@@ -1,16 +1,37 @@
+export type TypeUserResultWithTokenResponse = {
+  type: "success" | "error";
+  message: string;
+  user: {
+    token: string;
+    user: TypeUser;
+  };
+};
+
 export type TypeUser = {
-  id: number;
+  _id: number;
   username: string;
+  email: string;
   password: string;
 };
 
 export type TypeAuthResponse<T> = {
   ok: boolean;
   status: number;
-  type: "success" | "error";
-  message: string;
-  user?: T;
+  // type: "success" | "error";
+  // message: string;
+  // user?: T;
+  result?: T;
 };
+
+export type TypeAuthErrorResponse = {
+  type: "error";
+  message: string;
+};
+
+export interface TypeAuthLoginError extends TypeAuthErrorResponse {
+  ok: boolean;
+  status: number;
+}
 
 export type TypeResultResponse = Omit<
   TypeAuthResponse<TypeUser>,
